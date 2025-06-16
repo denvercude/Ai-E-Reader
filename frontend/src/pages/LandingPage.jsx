9a7c07dd41a4dfdff6196449215368a344a8f513
Denver <cudedenver@gmail.com>
2025-06-16 10:51:35 -0700

    chore(frontend): clean up starter code and create base folder strucutre

---
+++ frontend/src/pages/LandingPage.jsx
@@ -0,0 +1,10 @@
+function LandingPage(){
+    return (
+        <div>
+            <h1>Side Page</h1>
+            <p>Your intelligent reading companion.</p>
+        </div>
+    );
+}
+
+export default LandingPage;
