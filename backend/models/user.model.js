8eec141571af733b3477d0d64f3f502f6fd1dca1
denver <cudedenver@gmail.com>
2025-06-27 17:47:52 -0700

    feat(auth): add bcrypt password hashing to user model

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import bcrypt from 'bcrypt';
 
 const userSchema = new mongoose.Schema({
   username: {
@@ -20,5 +21,23 @@ const userSchema = new mongoose.Schema({
 }, {
   timestamps: true
 });
+
+userSchema.pre('save', async function (next) {
+  if(!this.isModified('password')) return next();
+
+  try {
+    const salt = await bcrypt.genSalt(10);
+
+    this.password = await bcrypt.hash(this.password, salt);
+
+    next();
+  } catch (error) {
+    next(error);
+  }
+});
+
+userSchema.methods.matchPassword = async function (enterPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
 
 export const User = mongoose.model('User', userSchema);
